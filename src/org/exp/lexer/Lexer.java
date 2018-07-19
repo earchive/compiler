bd2a445ba617d8dbc69252e40cc3074f9165d1ea
EULIR <runmingli_edu@163.com>
2018-07-19 23:57:14 +0800

    update lexer

--- src/org/exp/lexer/Lexer.java
+++ src/org/exp/lexer/Lexer.java
@@ -1,5 +1,6 @@
 package org.exp.lexer;
 
+import java.io.IOException;
 import java.util.Hashtable;
 
 public class Lexer {
@@ -13,5 +14,32 @@ public class Lexer {
 
 	public Lexer() {
 		reserve(new Word("if", Tag.IF));
+		reserve(new Word("else", Tag.ELSE));
+		reserve(new Word("while", Tag.WHILE));
+		reserve(new Word("do", Tag.DO));
+		reserve(new Word("break", Tag.BREAK));
+		reserve(Word.True);
+		reserve(Word.and);
+		reserve(Word.or);
+		reserve(Word.eq);
+		reserve(Word.ne);
+		reserve(Word.le);
+		reserve(Word.ge);
+		reserve(Word.minus);
+		reserve(Word.True);
+		reserve(Word.False);
+		reserve(Word.temp);
+	}
+
+	public void readch() throws IOException {
+		peek = (char) System.in.read();
+	}
+
+	public boolean readch(char c) throws IOException {
+		readch();
+		if (peek != c)
+			return false;
+		peek = ' ';
+		return true;
 	}
 }
