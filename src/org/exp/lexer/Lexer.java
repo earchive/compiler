1d999e1d2f14f2cc7f27f553caba9f6271c4e89d
EULIR <runmingli_edu@163.com>
2018-07-21 23:58:58 +0800

    complete lexer

--- src/org/exp/lexer/Lexer.java
+++ src/org/exp/lexer/Lexer.java
@@ -42,4 +42,65 @@ public class Lexer {
 		peek = ' ';
 		return true;
 	}
+
+	public Token scan() throws IOException {
+		for (; ; readch()) {
+			if (peek == ' ' || peek == '\t') continue;
+			else if (peek == '\n') line++;
+			else break;
+		}
+		switch (peek) {
+			case '&':
+				if (readch('&')) return Word.and;
+				else return new Token('&');
+			case '|':
+				if (readch('|')) return Word.or;
+				else return new Token('|');
+			case '=':
+				if (readch('=')) return Word.eq;
+				else return new Token('=');
+			case '!':
+				if (readch('=')) return Word.ne;
+				else return new Token('!');
+			case '<':
+				if (readch('=')) return Word.le;
+				else return new Token('<');
+			case '>':
+				if (readch('=')) return Word.ge;
+				else return new Token('>');
+		}
+		if (Character.isDigit(peek)) {
+			int v = 0;
+			do {
+				v = 10 * v + Character.digit(peek, 10);
+				readch();
+			} while (Character.isDigit(peek));
+			if (peek != ',') return new Num(v);
+			double x = v;
+			double d = 10D;
+			for (; ; ) {
+				readch();
+				if (!Character.isDigit(peek)) break;
+				x += Character.digit(peek, 10) / d;
+				d *= 10;
+			}
+			return new Real(x);
+		}
+		if (Character.isLetter(peek)) {
+			StringBuilder b = new StringBuilder();
+			do {
+				b.append(peek);
+				readch();
+			} while (Character.isLetterOrDigit(peek));
+			String s = b.toString();
+			Word w = (Word) words.get(s);
+			if (w != null) return w;
+			w = new Word(s, Tag.ID);
+			words.put(s, w);
+			return w;
+		}
+		Token tok = new Token(peek);
+		peek = ' ';
+		return tok;
+	}
 }
